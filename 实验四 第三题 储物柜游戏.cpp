56d11f9604451a74c49444f92c34ad5c95a2590c
Lam Zising <941435991@qq.com>
2024-12-01 10:55:22 +0800

    Add files via upload

---
+++ 实验四 第三题 储物柜游戏.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	bool a[101];
+	for (int i = 1; i <= 100; ++i)
+		for (int j = 1; i * j <= 100; ++j)
+			a[i * j] = !a[i * j];
+	for (int i = 1; i <= 100; ++i)
+		if (a[i]) cout << i << " ";
+	return 0;
+}
---
+++ 实验四 第二题 起泡排序.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+void bubble_sort(double arr[10])
+{
+    for (int i = 1; i <= 10; i++)
+        for (int j = 0; j < 9; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                double tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+}
+int main()
+{
+    double array[10];
+    cout << "Before bubble sort: " << endl;
+    for (int i = 0; i < 10; i++)
+        cin >> array[i];
+    bubble_sort(&array[10]);
+    cout << "After bubble sort: " << endl;
+    for (int i = 0; i < 10; i++)
+        cout << array[i] << "\t";
+    return 0;
+}
