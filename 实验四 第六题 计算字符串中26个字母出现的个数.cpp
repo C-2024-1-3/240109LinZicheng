33d37c408db9fc956889341bf770c25362262fcb
Lam Zising <941435991@qq.com>
2024-12-01 10:56:06 +0800

    Add files via upload

---
+++ 实验四 第六题 计算字符串中26个字母出现的个数.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	string input;
+	int counts[26];
+	for (int i = 0; i < 26; i++)
+		counts[i] = 0;
+	cout << "Input a string of letters: " << endl;
+	getline(cin, input);
+	for (char ch : input) // This sentence is used to ergodic each element in an array or string.
+	{
+		char lowerCh = tolower(static_cast <unsigned char>(ch));
+		if (lowerCh >= 'a' && lowerCh <= 'z')
+			counts[lowerCh - 'a']++;
+	}
+	for (char ch = 'a'; ch <= 'z'; ++ch)
+	{
+		if (counts[ch - 'a'] == 0)
+			continue; // If this condition is true, do no operation and directly jump to the next sentence.
+		else
+			cout << ch << ":" << counts[ch - 'a'] << endl;
+	}
+	return 0;
+}
